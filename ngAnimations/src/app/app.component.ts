47b58f471c49851eeca1ed6282a455eca5452638
Adam Seers <131391702+adamseers-ecole@users.noreply.github.com>
2025-12-09 17:13:32 -0500

    FCT Animations

--- ngAnimations/src/app/app.component.ts
+++ ngAnimations/src/app/app.component.ts
@@ -6,11 +6,51 @@ import { bounce, shakeX, tada } from 'ng-animate';
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    standalone: true
+    standalone: true,
+  animations:[
+    trigger('shake', [transition(':increment', useAnimation(shakeX, {
+      params: { timing: 2.0 }
+    }))]),
+    trigger('bounce', [transition(':increment', useAnimation(bounce, {
+      params: { timing: 4.0 }
+    }))]),
+    trigger('tada', [transition(':increment', useAnimation(tada, {
+      params: { timing: 3.0 }
+    }))])
+  ]
 })
 export class AppComponent {
   title = 'ngAnimations';
 
+  rotate= false;
+
+  first = 0;
+  second = 0;
+  third = 0;
+
   constructor() {
+  }
+
+  rotateCube() {
+    if(this.rotate == false){
+      this.rotate = true;
+      setTimeout(() => {this.rotate = false;},2000);
+    }
+  }
+
+  playNgAnimations(forever:boolean) {
+    this.first++;
+    setTimeout(() => {
+      this.second++;
+      setTimeout(() => {
+        this.third++;
+        if(forever)
+        {
+          setTimeout(() => {
+            this.playNgAnimations(true);
+          },3000);
+        }
+      },3000);
+    },2000);
   }
 }
